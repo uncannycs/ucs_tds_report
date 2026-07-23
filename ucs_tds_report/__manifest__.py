847a8e84992cdb585f0786455d1d377040f83e52
lc-ucs <lcodooucs@gmail.com>
2026-07-23 19:22:26 +0530

    [17.0] ucs_tds_report

---
+++ ucs_tds_report/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'UCS TDS Report',
+    'version': '17.0.1.0.0',
+    'summary': 'TDS Transaction Report Generator in Excel',
+    'description': """
+        Generates consolidated TDS transaction details (Excel) by selecting a date period and relevant TDS accounts.
+    """,
+    'category': 'Accounting/Reporting',
+    'author': 'Uncanny Consulting Services LLP',
+    'depends': ['account', 'l10n_in'],
+    'data': [
+        'security/security_groups.xml',
+        'security/ir.model.access.csv',
+        'wizard/tds_report_wizard_view.xml',
+    ],
+    'images': ["static/description/banner.gif"],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ ucs_tds_report/wizard/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import tds_report_wizard
